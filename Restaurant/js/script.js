3a928b94eee4a0d55e98a090b13e573eef30f2da
luissibaja <luissibaja@gmail.com>
2016-07-10 17:12:35 -0600

    upload 6

--- Restaurant/js/script.js
+++ Restaurant/js/script.js
@@ -94,6 +94,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 	      // variable's name implies it expects.
 	      // var chosenCategoryShortName = ....
 	      var category = chooseRandomCategory (categories);
+	      console.log(categories);
 	      var chosenCategoryShortName = "'" + category.short_name + "'";
 		  var homeHtmlToInsertIntoMainPage = homeHtml;
 		  insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", 'SP');
